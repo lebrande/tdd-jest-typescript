8c1494140fe273863b05a572b885aeb9d045c1b4
Jakub Pusiak <jakub.pusiak@gmail.com>
2020-04-06 00:57:49 +0200

    Pass first test.

--- lib/formatCurrency.ts
+++ lib/formatCurrency.ts
@@ -1,3 +1,7 @@
-export const formatCurrency = () => {
-  return '';
+export const formatCurrency = (cents: number) => {
+  const [cent1, cent2, ...rest] = `${cents}`
+    .split('')
+    .reverse();
+  const result = `${rest.reverse().join('')}.${cent2}${cent1}`;
+  return result;
 };
